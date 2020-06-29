18ab00b1148a7edf385acd9bbcfb68add9d68b29
James Sherry <james.m.sherry@googlemail.com>
2020-06-29 18:08:28 +0100

    removed tab move

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 3333;
 const apiRoot = "/api/";
 const version = "v1";
 const fullAPIRoot = apiRoot + version;
